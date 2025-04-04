6089a2e2d1e25be84dac952374f7a720498237cc
castilemobilebot <79207633+castilemobilebot@users.noreply.github.com>
2025-04-04 11:40:44 +0700

    Update event.js

--- scripts/event.js
+++ scripts/event.js
@@ -1,13 +1,15 @@
-// Event untuk tombol "Tonton Iklan +5"
+console.log("event.js berhasil dimuat!");
+
+// Event listener untuk tombol "Tonton Iklan +5"
 document.querySelector('button[onclick="showRewardedAd()"]').addEventListener('click', showRewardedAd);
 
-// Event untuk tombol "Withdraw"
+// Event listener untuk tombol "Withdraw"
 document.querySelector('button[onclick="withdraw()"]').addEventListener('click', withdraw);
 
-// Event untuk tombol "Riwayat Withdraw"
+// Event listener untuk tombol "Riwayat Withdraw"
 document.querySelector('button[onclick="viewHistory()"]').addEventListener('click', viewHistory);
 
-// Event untuk tombol "Kembali ke Menu"
+// Event listener untuk tombol "Kembali ke Menu"
 document.querySelector('button[onclick="goBack()"]').addEventListener('click', goBack);
 
 console.log("Event listeners berhasil diatur.");
